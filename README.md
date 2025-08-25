819d912f08d96276c699f186dee17b10552baedb
SRIKAR LANKALAPALLI <srikarlankalapalli@gmail.com>
2025-08-25 19:42:18 -0400

    push2

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
-Starts with Package : every folder starts with package So the complier know where set of instructions are being instialzed over a particular package 
+21 Aug Assignment 
 
-fmt:It is used to know how we can print the values and read the values from  user stand point 
-Example : fmt.Println("Hello world") So Here the complier look fo fmt and verify whether this particual function (Println) is present in fmt or not 
-Note : fmt never takes empty argument 
+1. Write a Palindrome function in Go(We checks if string reads the same forward and backwards)
+   Here we compare the first and last runes and move inward
 
-func: It says to compiler to execute the statement thats your passing inside of the particular function 
-Argument: It represent with this () 
-Note : func main () Here the argument can be none or Multiple
+2. By using Go routines, Print Hello in  different languages 
+   Here we start mutiple goroutines and use a syc.WaitGroup to block until they are done
 
-go run : it is command used to run your Go program 
+3. Write a Factorial program by using Go 
+   Computes n! with a simple loop
+
+4. Write fibonacci sequence in Go
+   Prints the first N Fibonacci numbers using two running variables (a, b = b, a+b)
+
+5. Write a word counting in Go
--- main.go
+++ main.go
@@ -1,9 +1,35 @@
 package main
 
 import (
+	"sync"
+	"strings"
 	"GO-18-08/simplecalc"
 	"fmt"
 )
+
+func isPalindrome(s string) bool {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < j; i, j = i+1, j-1 {
+		if r[i] != r[j] {
+			return false
+		}
+	}
+	return true
+}
+
+func say(lang, text string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Printf("%s: %s\n", lang, text)
+}
+
+func factorial(n uint64) uint64 {
+	var res uint64 = 1
+	for i := uint64(2); i <= n; i++ {
+		res *= i
+	}
+	return res
+}
+
 // Structure
 type Person struct {
 	Name string
@@ -42,6 +68,33 @@ func Transfer(from, to *Account, amt int) bool {
 	return true
 }
 func main() {
+
+	fmt.Println(isPalindrome("level")) // true
+	fmt.Println(isPalindrome("hello")) // false
+
+	hellos := map[string]string{
+		"English": "Hello",
+		"Spanish": "Hola",
+		"French":  "Bonjour",
+		"Hindi":   "Namaste",
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(len(hellos))
+	for lang, text := range hellos {
+		go say(lang, text, &wg)
+	}
+	wg.Wait()
+
+	fmt.Println(factorial(5)) 
+	fmt.Println(factorial(10))
+
+	text := "Go is fun and Go is fast"
+	counts := map[string]int{}
+	for _, w := range strings.Fields(text) {
+		counts[strings.ToLower(w)]++
+	}
+	fmt.Println(counts) 
 
 	//Map
 	accounts := make(map[string]*Account)  
